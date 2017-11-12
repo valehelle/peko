2bd5879e15d423824f6d2fed20572c150a65e49f
Hazmi Irfan Bin Zainal Ariffin <Peco@Hazmis-Mac-mini.local>
2017-11-12 17:09:05 +0800

    add relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable
+  has_many :posts
+  has_many :comments
   include DeviseTokenAuth::Concerns::User
 end
