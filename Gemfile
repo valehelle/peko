b4e1f3a79fbf9df62f4ddbbc72ff34c43bc0645a
hazmi irfan <hazmi@lelong.com.my>
2017-11-13 15:32:14 +0800

    add elastic

--- Gemfile
+++ Gemfile
@@ -22,6 +22,9 @@ gem 'uglifier', '>= 1.3.0'
 gem 'devise_token_auth'
 #for location
 gem 'geocoder'
+#For search
+gem 'elasticsearch-model'
+gem 'elasticsearch-rails'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,14 @@ class Post < ApplicationRecord
   has_many :comments
   reverse_geocoded_by :latitude, :longitude, :address => :location
   after_validation :reverse_geocode
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+  mapping do
+    indexes :question, type: 'string'
+    indexes :location, type: 'geo_point'
+  end
+
+  def as_indexed_json(options={})
+    as_json(only: 'question').merge location: { lat: self.latitude, lon: self.longitude }
+  end
 end
